30e3213b1562c772573e99053e5d26cee3328578
Gabriel Henrique Mombelli <gabriel.mombelli9@gmail.com>
2024-09-11 19:29:12 -0300

    Atividade finalizada

--- src/TrocaDeValores.java
+++ src/TrocaDeValores.java
@@ -1,9 +1,18 @@
 public class TrocaDeValores {
-    public static void main(String[] args) {
+        public static void main(String[] args) {
 
-        int vetor[] = {5, 10};
+            int[] num = new int[2];
+            num[0] = 5;
+            num[1] = 10;
 
-        System.out.println();
+            System.out.println(num[0] + " " + " " + num[1]);
 
+            int aux = num[0];
+            num[0] = num[1];
+            num[1] = aux;
+
+            System.out.println(num[0]+  " `" + " " +  num[1]);
+
+
+        }
     }
-}
